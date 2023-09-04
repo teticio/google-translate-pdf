509b14fb42c037f5dcbe3f5aceb3f42336bf6b5d
Robert Dargavel Smith <teticio@gmail.com>
2023-09-04 17:18:29 +0100

    &&

--- run_docker.sh
+++ run_docker.sh
@@ -1,4 +1,4 @@
-docker build -t google-translate-pdf .
+docker build -t google-translate-pdf . && \
 docker run --rm --cpus 1 -m 1024m -p 9000:8080 \
     --read-only --tmpfs /tmp:rw,exec,size=512m \
     google-translate-pdf
